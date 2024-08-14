d14460c627a98caf04c52d93eb24e1dc33772d29
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-08-14 15:44:57 +0100

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,60 +10,33 @@ from typing import Callable
 import redis
 import requests
 
-redis_client = redis.Redis()
-
+redis_store = redis.Redis()
 """
 The module-level Redis instance.
 """
 
 
-def cache_with_expiration(method: Callable) -> Callable:
-    """
-    Decorator to cache the result of a function with an expiration time.
-
-    Args:
-        expiration (int): The expiration time in seconds.
-
-    Returns:
-        Callable: The wrapped function with caching.
-    """
+def data_cacher(method: Callable) -> Callable:
+    """Caches the output of fetched data."""
 
     @wraps(method)
-    def wrapper(url: str) -> str:
-        """
-        Wrapper function to handle caching and expiration.
-
-        Args:
-            url (str): The URL to fetch.
-
-        Returns:
-            str: The HTML content of the URL, either from cache or fetched.
-        """
-        redis_client.incr(f"count:{url}")
-        result = redis_client.get(f"result:{url}")
-
+    def invoker(url) -> str:
+        """The wrapper function for caching the output."""
+        redis_store.incr(f"count:{url}")
+        result = redis_store.get(f"result:{url}")
         if result:
             return result.decode("utf-8")
-
         result = method(url)
-        redis_client.set(f"count:{url}", 0)
-        redis_client.setex(f"result:{url}", 10, result)
-
+        redis_store.set(f"count:{url}", 0)
+        redis_store.setex(f"result:{url}", 10, result)
         return result
 
-    return wrapper
+    return invoker
 
 
-@cache_with_expiration
+@data_cacher
 def get_page(url: str) -> str:
+    """Returns the content of a URL after caching the request's response,
+    and tracking the request.
     """
-    Obtain the HTML content of a particular URL.
-
-    Args:
-        url (str): The URL to fetch.
-
-    Returns:
-        str: The HTML content of the URL.
-    """
-    response = requests.get(url)
-    return response.text
+    return requests.get(url).text
